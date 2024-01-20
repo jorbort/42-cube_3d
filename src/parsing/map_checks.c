d3628cf833d3038b271f38ea023106cc1e28b8e8
Jorge Bortolotti Suarez <jbortolo@cbr4s2.42barcelona.com>
2024-01-20 18:36:30 +0100

    hay problemas con el map checker si el mapa no esta cerrado a partir de la tercera fila

--- src/parsing/map_checks.c
+++ src/parsing/map_checks.c
@@ -6,7 +6,7 @@
 /*   By: jbortolo <jbortolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 19:16:31 by jbortolo          #+#    #+#             */
-/*   Updated: 2024/01/20 13:35:02 by jbortolo         ###   ########.fr       */
+/*   Updated: 2024/01/20 17:11:59 by jbortolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ bool	check_top_bottom(t_map *map)
 
 	while (map->map[0][i])
 	{
-		if (!is_valid_char(map->map[i][j]))
+		if (!is_valid_char(map->map[0][j]))
 			return (false);
 		if (map->map[0][i] == ' ' && !is_valid_char(map->map[1][i]))
 			return (false);
@@ -46,6 +46,43 @@ bool	check_top_bottom(t_map *map)
 		j++;
 	}
 	return (true);
+}
+/*bool	check_sides(t_map *map)
+{
+	 int	i;
+	 int	j;
+
+	 i = 0;
+	 j = 0;
+	 while (map->map[i])
+	 {
+		while(map->map[i][j])
+		{
+			if (!is_valid_char(map->map[i][j]))
+			i++;
+		}
+		j++;
+	 }
+}*/
+
+bool	check(char **map)
+{
+	static int	i = 0;
+	static int	j = 0;
+
+	while (map[i])
+	{
+		while (map[i][j])
+		{
+			while (map[i][j] == ' ' || map[i][j] == '\t')
+				j++;
+			if (i == 0 || i == (ft_arrlen(map) - 1))
+				if (!is_valid_char(map[i][j]))
+					return (false);
+			
+		}
+	}
+
 }
 
 bool	check_walls(t_map *map)

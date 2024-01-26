84b2329771e7af7b13653d1cb0ecf287906134fe
Jorge Bortolotti Suarez <jbortolo@car15s4.42barcelona.com>
2024-01-26 10:39:57 +0100

    un poco de limpieza

--- mouse_maping.c
+++ mouse_maping.c
@@ -6,7 +6,7 @@
 /*   By: jbortolo <jbortolo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 17:48:43 by jbortolo          #+#    #+#             */
-/*   Updated: 2024/01/25 19:48:50 by jbortolo         ###   ########.fr       */
+/*   Updated: 2024/01/25 20:08:53 by jbortolo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,6 @@
 
 int	mouse_moving(int x, int y, t_program *game)
 {
-	printf("%d-%d\n", x, y);
 	if (x < (WIN_WIDTH / 2) + 600
 		&& !is_in_bounds(game->map->start_x
 			- (GRID_SIZE / 12), game->map->start_y, game))
